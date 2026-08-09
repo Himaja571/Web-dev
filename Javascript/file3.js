721aeaa74b0fa330edb9117509a0c8ef29fe1833
Himaja Chanda <himajapatel0571@gmail.com>
2026-08-09 23:05:05 +0530

    Intial commit

---
+++ Javascript/file3.js
@@ -0,0 +1,11 @@
+let num1=1.5e2
+console.log(num1)
+console.log(2)
+let firstName="water"
+let lastName="melon"
+let fullName=firstName+" "+lastName
+console.log(fullName)
+console.log(3)
+let num=String(6)
+console.log(num,typeof num)
+
