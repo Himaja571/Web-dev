ff2afbeb4e2c19debd60cd8e7ac43fd58a6e83c9
Himaja Chanda <himajapatel0571@gmail.com>
2026-08-09 23:08:19 +0530

    Intial commit

---
+++ Javascript/file4.js
@@ -0,0 +1,12 @@
+let num1=5
+let num2=3
+let sum=num1+num2
+console.log(sum)
+let difference=num1-num2
+console.log(difference)
+let product=num1*num2   
+console.log(product)
+let quotient=num1/num2
+console.log(quotient)
+let remainder=num1%num2
+console.log(remainder)  
